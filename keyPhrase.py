1ed6fbbe7fc574ff2ea4bbb59346725737e3fdfa
Kiryl <bagushevich2003@mail.ru>
2023-04-07 13:26:51 +0300

    v2

--- keyPhrase.py
+++ keyPhrase.py
@@ -1,35 +1,73 @@
 import string
-
+from textwrap import wrap
+from copy import deepcopy
 
 def number_key(key_phrase):
-    letters = tuple(string.ascii_lowercase)
+    key_list = list(key_phrase)
+    sorted_list_key = sorted(key_phrase)
+    sorted_key = ''.join(sorted_list_key)
+    deepcopy_sorted_list_key = deepcopy(sorted_list_key)
+    keyword_indexes = []
+    for i in range(len(key_list)):
+        symbol_index = deepcopy_sorted_list_key.index(key_list[i])
+        keyword_indexes.append(symbol_index)
+        deepcopy_sorted_list_key[symbol_index] = "Q"
 
-    key_phrase = key_phrase.lower()
-    key_number = [i for i in key_phrase]
+    # letters = tuple(string.ascii_lowercase)
+    #
+    # key_phrase = key_phrase.lower()
+    # key_number = list(key_phrase)
+    #
+    # counter = 0
+    #
+    # for letter_index, letter_value in enumerate(letters):
+    #     for key_index, key_value in enumerate(key_phrase):
+    #         if letter_value == key_value:
+    #             key_number[key_index] = counter
+    #             counter += 1
 
-    counter = 1
-
-    for letter_index, letter_value in enumerate(letters):
-        for key_index, key_value in enumerate(key_phrase):
-            if letter_value == key_value:
-                key_number[key_index] = counter
-                counter += 1
-
-    return key_number
+    return keyword_indexes
 
 
 def key_phrase_encryption(text, key):
     key = number_key(key)
-    text_array = [text[i:i+(len(key))] for i in range(0, len(text), (len(text) // (len(key))))]
+    len_key = len(key)
+
+    text_array = wrap(text.replace(' ', '!'), len_key)
+    if len(text_array) != len_key:
+        text_array[-1] = text_array[-1].ljust(len_key, '@')
 
     chiphered_text = []
+    for i in range(len(text_array)):
+        deepcopy_key = deepcopy(key)
+        for j in range(len(deepcopy_key)):
+            chiphered_text.append(text_array[i][deepcopy_key.index(min(deepcopy_key))])
+            deepcopy_key[deepcopy_key.index(min(deepcopy_key))] = 100
+    result = ''.join(chiphered_text)
+    return result
 
-    for index, value in enumerate(text_array):
-        for i in range(len(value)):
-            for index_key, item in enumerate(key):
-                if i == item:
-                    chiphered_text.append(value[index_key])
+
+def key_phrase_decryption(text, key):
+    key = number_key(key)
+    len_key = len(key)
+
+    text_array = wrap(text, len_key)
+    if len(text_array) != len_key:
+        text_array[-1] = text_array[-1].ljust(len_key, '@')
+
+    dechiphered_text = []
+
+    for i in range(len(text_array)):
+        for j in range(len(key)):
+            dechiphered_text.append(text_array[i][key[j]])
+
+    result = ''.join(dechiphered_text)
+
+    return result.replace('!', ' ').replace('@', ' ')
 
 
 if __name__ == '__main__':
-    print(number_key('KiRyl'))
+
+    a = key_phrase_encryption('This is a lecture on encryption algorithms', 'cryptography')
+    # print(a, sep='\n')
+    print(a, key_phrase_decryption(a, 'cryptography'))
